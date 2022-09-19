b5415619bc9363eb243c7439563d9170f0c8523d
Kartik Yadav <13693180+kartiky9@users.noreply.github.com>
2022-09-19 22:02:51 +0530

    CSS updates + date functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // write your JavaScript here
+
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
+let daysElement = document.querySelector(".days");
+
+for (let i = 1; i <= 31; i++) {
+  let divElement = document.createElement("div");
+  let numberDayElement = divElement.appendChild("span");
+  numberDayElement.innerText = i;
+  numberDayElement.className = "day-span";
+  if (i === today.getDay()) {
+    numberDayElement.classList.add("selected-day");
+  }
+  daysElement.appendChild(divElement);
+}
